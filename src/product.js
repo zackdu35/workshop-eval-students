6a3015066129dc363188776b4bbb1606d53fdfbd
zack <remake35@gmail.com>
2017-02-09 12:10:21 +0100

    Exercise 4

--- src/product.js
+++ src/product.js
@@ -1,35 +1,43 @@
 var chalk = require('chalk');
 
-function Product(name, price) {
+function Product(name, price, runningTime, minDuration, maxDuration) {
   this.name = name;
   this.price = price;
-  this.toString = function(currency, rateCurrency) {
+  this.runningTime = runningTime;
+  this.minDuration = minDuration;
+  this.maxDuration = maxDuration;
+
+  this.toString = function(currency, rateCurrency, runningTime, minDuration, maxDuration) {
     var currency = currency || '$';
     var rateCurrency = rateCurrency || 1;
-    return chalk.yellow(this.name) + ' cost ' + chalk.red(this.price * rateCurrency) + currency;
+    return chalk.yellow(this.name) + ' cost ' + chalk.red(this.price * rateCurrency) + currency
+    + ' runningTime : ' + this.runningTime + ' minDuration : ' + this.minDuration + ' maxDuration : '+ this.maxDuration;
   }
 }
 
-function Book(name, price, isbn) {
-  Product.apply(this, [name, price]);
+function Book(name, price, isbn, minDuration, maxDuration) {
+  Product.apply(this, [name, price, null,minDuration, maxDuration]);
   this.isbn = isbn;
+
 }
 Book.prototype = Object.create(Product.prototype, {
   constructor: {value: Book}
 });
 
-function DVD(name, price, moovie) {
-  Product.apply(this, [name, price]);
+function DVD(name, price, moovie, runningTime) {
+  Product.apply(this, [name, price, runningTime,null,null]);
   this.moovie = moovie;
+
 }
 DVD.prototype = Object.create(Product.prototype, {
   constructor: {value: DVD}
 });
 
 
-function VideoGame(name, price, platform){
-  Product.apply(this, [name, price]);
+function VideoGame(name, price, platform, minDuration, maxDuration){
+  Product.apply(this, [name, price, null,minDuration, maxDuration]);
   this.platform = platform;
+
 }
 VideoGame.prototype = Object.create(Product.prototype, {
   constructor: {value: VideoGame}
--- src/store.js
+++ src/store.js
@@ -7,22 +7,22 @@ var service = require ('./rateExchangeService').service;
 
 var products = [
   // Books
-  new Book("Preacher Book One", 16.59, "978-1401240455"),
-  new Book('JavaScript: The Good Parts', 22.79, '978-0596517748'),
-  new Book('Alice In Wonderland', 6.99, '978-1619490222'),
-  new Book('Harry Potter and the Sorcerer\'s Stone', 6.85, '978-0439708180'),
+  new Book('Preacher Book One', 16.59, '978-1401240455', 80, 200),
+  new Book('JavaScript: The Good Parts', 22.79, '978-0596517748', 105, 265),
+  new Book('Alice In Wonderland', 6.99, '978-1619490222', 100, 190),
+  new Book('Harry Potter and the Sorcerer\'s Stone', 6.85, '978-0439708180', 140, 270),
   // DVDs
-  new DVD('The God, The Bad, and The Ugly', 12.99, true),
-  new DVD('Star Wars: A New Hope', 19.99, true),
-  new DVD('Rogue One: A Star Wars Story', 17.39, true),
-  new DVD('Alice In Wonderland', 14.99, true),
-  new DVD('The Wire Season 1', 29.99, false),
-  new DVD('Breaking Bad: Season 05', 16.49, false),
-  new DVD('Freaks and Geeks: The Complete series', 54.99, false),
-    // VideoGames
-  new VideoGame('FIFA 17', 39.56, 'PlayStation 4'),
-  new VideoGame('XCOM: Enemy Unknown', 15.00, 'PC'),
-  new VideoGame('Super Smash Bros Melee', 63.90, 'Game Cube')
+  new DVD('The God, The Bad, and The Ugly', 12.99, true, 178),
+  new DVD('Star Wars: A New Hope', 19.99, true, 124),
+  new DVD('Rogue One: A Star Wars Story', 17.39, true, 120),
+  new DVD('Alice In Wonderland', 14.99, true, 108),
+  new DVD('The Wire Season 1', 29.99, false, 780),
+  new DVD('Breaking Bad: Season 05', 16.49, false, 375),
+  new DVD('Freaks and Geeks: The Complete series', 54.99, false, 756),
+  // VideoGames
+  new VideoGame('FIFA 17', 39.56, 'PlayStation 4', 400, 1600),
+  new VideoGame('XCOM: Enemy Unknown', 15.00, 'PC', 300, 1800),
+  new VideoGame('Super Smash Bros Melee', 63.90, 'Game Cube', 300, 2400)
 
 ];
 
@@ -48,10 +48,10 @@ function calculateTotalprice(products) {
   return truncateDecimals(sum,2);
 }
 
-function toString(currency, rateCurrency) {
+function toString(currency, rateCurrency, runningTime, minDuration, maxDuration) {
   var result = chalk.green('The store is open, ' + products.length + ' articles are available :');
   for (var i = products.length - 1; i >= 0; i--) {
-    result += '\n - ' + products[i].toString(currency, rateCurrency);
+    result += '\n - ' + products[i].toString(currency, rateCurrency, runningTime, minDuration, maxDuration);
   }
   return result;
 }
